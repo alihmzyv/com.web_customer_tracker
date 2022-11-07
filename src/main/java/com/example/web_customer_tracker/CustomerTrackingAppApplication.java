6d397b8530027c5e0d4a5631dc68b7b52979a176
alihmzyv <alihmzyv@gmail.com>
2022-11-07 21:19:33 +0400

    optimized imports

--- src/main/java/com/example/web_customer_tracker/CustomerTrackingAppApplication.java
+++ src/main/java/com/example/web_customer_tracker/CustomerTrackingAppApplication.java
@@ -1,14 +1,8 @@
 package com.example.web_customer_tracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 @ImportResource("classpath:spring-mvc-demo.xml")
