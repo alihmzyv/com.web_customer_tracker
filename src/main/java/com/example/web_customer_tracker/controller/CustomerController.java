e0d2cea883d0ce71bf6da783489abc244fd9aa73
alihmzyv <alihmzyv@gmail.com>
2022-11-07 12:11:28 +0400

    replaced CustomerDao with CustomerService, added methods

--- src/main/java/com/example/web_customer_tracker/controller/CustomerController.java
+++ src/main/java/com/example/web_customer_tracker/controller/CustomerController.java
@@ -1,28 +1,53 @@
 package com.example.web_customer_tracker.controller;
 
 
-import com.example.web_customer_tracker.dao.CustomerDao;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import com.example.web_customer_tracker.entity.Customer;
+import com.example.web_customer_tracker.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
+import javax.validation.Valid;
 
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
     @Autowired
-    private CustomerDao customerDao;
+    private CustomerService customerService;
 
     @GetMapping("/list")
     public String showAllCustomers(Model model){
-        model.addAttribute("allCustomers", customerDao.getCustomers());
+        model.addAttribute("allCustomers", customerService.getCustomers());
         return "list-customers";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        model.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute @Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        customerService.saveOrUpdateCustomer(customer);
+        return "redirect:/customer/list";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String updateCustomer(Model model, @PathVariable(name = "id") Long id) {
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer-form";
+    }
+
+    @GetMapping("/delete/{id}") //not good to use get method for deleting
+    public String deleteCustomer(@PathVariable(name = "id") Long id) {
+        customerService.deleteById(id);
+        return "redirect:/customer/list";
     }
 }
