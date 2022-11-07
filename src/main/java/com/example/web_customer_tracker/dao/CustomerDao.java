f4b4b32531e3a0d2a28a0d2aa429fe655a121106
alihmzyv <alihmzyv@gmail.com>
2022-11-07 12:11:41 +0400

    added the rest of actions

--- src/main/java/com/example/web_customer_tracker/dao/CustomerDao.java
+++ src/main/java/com/example/web_customer_tracker/dao/CustomerDao.java
@@ -5,5 +5,8 @@ import com.example.web_customer_tracker.entity.Customer;
 import java.util.List;
 
 public interface CustomerDao {
+    Customer getCustomerById(Long id);
     List<Customer> getCustomers();
+    void saveOrUpdateCustomer(Customer customer);
+    void deleteById(Long id);
 }
